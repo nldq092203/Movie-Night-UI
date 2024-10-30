b52f5a93516c0f2f6e6c8892b2727f052599e740
nldq092203 <lnguye220903@gmail.com>
2024-10-30 15:29:36 +0100

    package-json

--- src/config.jsx
+++ src/config.jsx
@@ -1,1 +1,2 @@
-export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "https://web-production-5212b.up.railway.app";
+console.log(import.meta.VITE_API_BASE_URL)
+export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
