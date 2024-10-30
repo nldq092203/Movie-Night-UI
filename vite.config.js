646c92d4b1f1a06637fc90bf96cb1535bb608f87
nldq092203 <lnguye220903@gmail.com>
2024-10-30 12:14:27 +0100

    Railway deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',  // Listen on all network interfaces
-    port: 5173,       // Specify the port (if different from 5173)
-    strictPort: true, // Ensure Vite uses the specified port or fails
+    port: process.env.PORT || 3000, 
   },
 });
