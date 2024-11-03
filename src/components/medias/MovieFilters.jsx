92db7f56e82bfb8010238c460bfe15a641d85460
nldq092203 <lnguye220903@gmail.com>
2024-11-03 03:09:08 +0100

    Edit filter

--- src/components/medias/MovieFilters.jsx
+++ src/components/medias/MovieFilters.jsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import { debounce } from 'lodash';
 import { apiBaseUrl } from '../../config';
+import { Button, Tooltip, Input, Divider, useMantineTheme } from '@mantine/core';
+import { IconChevronDown, IconChevronUp, IconX } from '@tabler/icons-react';
 
 function Filters({ filters, setFilters, ordering, setOrdering }) {
+  const theme = useMantineTheme(); // Access the current theme
   const [genreOptions, setGenreOptions] = useState([]);
-  const [visibleGenres, setVisibleGenres] = useState(6); // Number of visible genres initially
+  const [visibleGenres, setVisibleGenres] = useState(6);
   const [isOpen, setIsOpen] = useState(false);
-  const [imdbRating, setImdbRating] = useState([0, 10]); // IMDb Rating slider state
+  const [imdbRating, setImdbRating] = useState([0, 10]);
   const [isYearDropdownOpen, setIsYearDropdownOpen] = useState(false);
 
-  // Fetch genres from the API to populate the genre filter options
+  // Fetch genres from API
   useEffect(() => {
     const fetchGenres = async () => {
       try {
@@ -23,7 +26,7 @@ function Filters({ filters, setFilters, ordering, setOrdering }) {
     fetchGenres();
   }, []);
 
-  // Toggle selected genre in filters
+  // Handle genre selection
   const handleGenreClick = (genre) => {
     setFilters((prevFilters) => ({
       ...prevFilters,
@@ -33,24 +36,21 @@ function Filters({ filters, setFilters, ordering, setOrdering }) {
     }));
   };
 
-  // Show more or less genres
+  // Toggle genre visibility
   const toggleVisibleGenres = () => {
     setVisibleGenres((prev) => (prev === 6 ? genreOptions.length : 6));
   };
 
+  // Handle general input change
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFilters((prevFilters) => {
-      const updatedFilters = { ...prevFilters, [name]: value };
-      return updatedFilters;
-    });
-  };
-  // Handle ordering changes
-  const handleOrderingChange = (e) => {
-    setOrdering(e.target.value);
+    setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
   };
 
-  // Debounced function to update filters only once when the user stops sliding
+  // Handle ordering changes
+  const handleOrderingChange = (e) => setOrdering(e.target.value);
+
+  // Debounced function to update IMDb rating
   const debouncedUpdateFilters = useCallback(
     debounce((newRating) => {
       setFilters((prevFilters) => ({
@@ -58,103 +58,90 @@ function Filters({ filters, setFilters, ordering, setOrdering }) {
         imdb_rating_from: newRating,
       }));
     }, 300),
-    [] // The empty dependency array ensures debounce is created only once
+    []
   );
 
-  // Handle IMDb rating change and trigger debounced update
+  // Handle IMDb rating change
   const handleImdbRatingChange = (value) => {
     setImdbRating(value);
-    debouncedUpdateFilters(value); // Update filters with debounce
+    debouncedUpdateFilters(value);
   };
 
+  const bgColor = theme.colorScheme === 'dark' ? '#000000' : '#ffffff';
+
   return (
-    <div className="space-y-4 p-6 bg-black text-white rounded-lg shadow-md max-w-lg mx-auto">
+    <div style={{ backgroundColor: bgColor }} className="space-y-4 p-6 text-white rounded-lg shadow-lg max-w-xl mx-auto">
       <h3 className="text-xl font-semibold mb-4 text-center">Filters</h3>
 
       {/* Genres Section */}
-      <div className="space-y-4">
+      <div>
         <h4 className="font-bold mb-2">Genres</h4>
         <div className="flex flex-wrap gap-2">
           {genreOptions.slice(0, visibleGenres).map((genre) => (
-            <button
-              key={genre.id}
-              onClick={() => handleGenreClick(genre.name)}
-              className={`flex items-center justify-center px-4 py-2 border rounded-full text-sm 
-                ${filters.genres.includes(genre.name) ? 'bg-white text-black' : 'border-gray-300 text-white'}`}
-            >
-              {genre.name}
-            </button>
+            <Tooltip label="Select genre" position="bottom" key={genre.id}>
+              <Button
+                variant={filters.genres.includes(genre.name) ? 'filled' : 'outline'}
+                size="xs"
+                onClick={() => handleGenreClick(genre.name)}
+                style={{
+                  backgroundColor: filters.genres.includes(genre.name) ? '#add8e6' : 'transparent',
+                  color: filters.genres.includes(genre.name) ? 'black' : '#add8e6',
+                  borderColor: '#add8e6',
+                }}
+              >
+                {genre.name}
+              </Button>
+            </Tooltip>
           ))}
         </div>
         {genreOptions.length > 6 && (
-          <button
+          <Button
             onClick={toggleVisibleGenres}
-            className="mt-2 px-4 py-2 border rounded-full text-sm flex items-center justify-center"
+            variant="subtle"
+            size="xs"
+            leftIcon={visibleGenres === 6 ? <IconChevronDown /> : <IconChevronUp />}
+            color="blue"
+            style={{
+              marginTop: '20px',
+              color: '#add8e6',
+              borderColor: '#add8e6',
+            }}
           >
-            {visibleGenres === 6 ? 'Show more' : 'Show less'}{' '}
-            <span className="ml-2 transform transition-transform duration-200">
-              {visibleGenres === 6 ? '▼' : '▲'}
-            </span>
-          </button>
+            {visibleGenres === 6 ? 'Show more' : 'Show less'}
+          </Button>
         )}
       </div>
 
-      {/* Runtime Dropdown */}
-      <div className="relative space-y-2">
-        <h3 className="font-bold mb-2">Runtime (minutes)</h3>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="w-full flex justify-between items-center border rounded-md px-4 py-2 text-left text-white"
-        >
-          <span>Set Runtime</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`h-5 w-5 transition-transform ${isOpen ? 'rotate-180' : 'rotate-0'}`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 9l6 6 6-6" />
-          </svg>
-        </button>
-
-        {isOpen && (
-          <div className="absolute w-full mt-2 p-4 rounded-md bg-black shadow-lg z-10">
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label className="block text-sm font-semibold mb-1">Minimum Runtime</label>
-                <input
-                  type="number"
-                  name="runtime_minutes_from"
-                  value={filters.runtime_minutes_from || ''}
-                  onChange={handleInputChange}
-                  className="w-full border p-2 rounded-md text-black"
-                  placeholder="Min Runtime"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-semibold mb-1">Maximum Runtime</label>
-                <input
-                  type="number"
-                  name="runtime_minutes_to"
-                  value={filters.runtime_minutes_to || ''}
-                  onChange={handleInputChange}
-                  className="w-full border p-2 rounded-md text-black"
-                  placeholder="Max Runtime"
-                />
-              </div>
-            </div>
-          </div>
-        )}
+      {/* Runtime Input */}
+      <Divider my="sm" color="blue" />
+      <div className="space-y-2">
+        <h4 className="font-bold mb-2">Runtime (minutes)</h4>
+        <div className="grid grid-cols-2 gap-2">
+          <Input
+            type="number"
+            name="runtime_minutes_from"
+            value={filters.runtime_minutes_from || ''}
+            onChange={handleInputChange}
+            placeholder="Min"
+            variant="filled"
+            styles={{ input: { borderColor: 'blue' } }}
+          />
+          <Input
+            type="number"
+            name="runtime_minutes_to"
+            value={filters.runtime_minutes_to || ''}
+            onChange={handleInputChange}
+            placeholder="Max"
+            variant="filled"
+            styles={{ input: { borderColor: 'blue' } }}
+          />
+        </div>
       </div>
 
-      {/* IMDb Rating Slider */}
+      {/* IMDb Rating */}
+      <Divider my="sm" color="blue" />
       <div>
         <h4 className="font-bold mb-2">Minimum IMDb Rating</h4>
-        <div className="flex items-center justify-between mb-4">
-          <span>0</span>
-          <span>10</span>
-        </div>
         <input
           type="range"
           name="imdb_rating_from"
@@ -163,86 +150,59 @@ function Filters({ filters, setFilters, ordering, setOrdering }) {
           step="0.1"
           value={imdbRating}
           onChange={(e) => handleImdbRatingChange(parseFloat(e.target.value))}
-          className="w-full h-2 bg-pink-500 rounded-lg appearance-none"
+          className="w-full h-2 bg-blue-300 rounded-lg appearance-none"
         />
-        <div className="text-center mt-2 text-gray-500">
-          Selected Rating: {imdbRating}
-        </div>
+        <div className="text-center mt-2 text-blue-300">Selected Rating: {imdbRating}</div>
       </div>
+
       {/* Year Range */}
-      <div className="relative space-y-2">
-      <h3 className="font-bold mb-2">Year Range</h3>
-      <button
-        onClick={() => setIsYearDropdownOpen(!isYearDropdownOpen)}
-        className="w-full flex justify-between items-center border rounded-md px-4 py-2 text-left text-white"
-      >
-        <span>Set Year Range</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={`h-5 w-5 transition-transform ${isYearDropdownOpen ? 'rotate-180' : 'rotate-0'}`}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 9l6 6 6-6" />
-        </svg>
-      </button>
-
-      {isYearDropdownOpen && (
-        <div className="absolute w-full mt-2 p-4 rounded-md bg-black shadow-lg z-10">
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label className="block text-sm font-semibold mb-1">From</label>
-              <input
-                type="number"
-                name="published_from"
-                value={filters.published_from || ''}
-                onChange={handleInputChange}
-                className="w-full border p-2 rounded-md text-black"
-                placeholder="Start Year"
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-semibold mb-1">To</label>
-              <input
-                type="number"
-                name="published_to"
-                value={filters.published_to || ''}
-                onChange={handleInputChange}
-                className="w-full border p-2 rounded-md text-black"
-                placeholder="End Year"
-              />
-            </div>
-          </div>
+      <Divider my="sm" color="blue" />
+      <div>
+        <h4 className="font-bold mb-2">Year Range</h4>
+        <div className="grid grid-cols-2 gap-2">
+          <Input
+            type="number"
+            name="published_from"
+            value={filters.published_from || ''}
+            onChange={handleInputChange}
+            placeholder="From"
+            variant="filled"
+            styles={{ input: { borderColor: 'blue' } }}
+          />
+          <Input
+            type="number"
+            name="published_to"
+            value={filters.published_to || ''}
+            onChange={handleInputChange}
+            placeholder="To"
+            variant="filled"
+            styles={{ input: { borderColor: 'blue' } }}
+          />
         </div>
-      )}
-    </div>
-
-      {/* Clear All and Apply Buttons */}
-      <div className="flex justify-between items-center border-t pt-4">
-        <button
-          className="text-red-500 font-semibold"
-          onClick={() => {
-            // Reset all filters to their default values
-            setFilters({
-              genres: [],
-              country: '',
-              runtime_minutes_from: '',
-              runtime_minutes_to: '',
-              imdb_rating_from: '',
-              published_from: '',
-              published_to: '',
-              // Add other filters as needed
-            });
-            setOrdering(''); // Reset ordering as well if necessary
-            setImdbRating([0, 10]); // Reset IMDb rating slider
-            setVisibleGenres(6); // Reset genres visibility if needed
-            setBedrooms('Any'); // Reset bedroom filter if used
-          }}
-        >
-          Clear all
-        </button>
       </div>
+
+      {/* Clear All Button */}
+      <Button
+        variant="outline"
+        color="red"
+        className="mt-4 border border-red"
+        leftIcon={<IconX />}
+        onClick={() => {
+          setFilters({
+            genres: [],
+            runtime_minutes_from: '',
+            runtime_minutes_to: '',
+            imdb_rating_from: '',
+            published_from: '',
+            published_to: '',
+          });
+          setOrdering('');
+          setImdbRating([0, 10]);
+          setVisibleGenres(6);
+        }}
+      >
+        Clear All
+      </Button>
     </div>
   );
 }
--- src/components/navigations/FilterDrawer.jsx
+++ src/components/navigations/FilterDrawer.jsx
@@ -1,48 +1,63 @@
 import React from 'react';
 import Filters from '../medias/MovieFilters';
 import { useDisclosure } from '@mantine/hooks';
-import { Drawer, Button } from '@mantine/core';
+import { Drawer, Button, Group } from '@mantine/core';
 import { IconAdjustmentsHorizontal } from '@tabler/icons-react';
 
-function FilterDrawer({ filters, setFilters, ordering, setOrdering, theme}) {
+function FilterDrawer({ filters, setFilters, ordering, setOrdering, theme }) {
   const [opened, { open, close }] = useDisclosure(false);
+
   // Determine styles based on the current theme
   const isDarkMode = theme.colorScheme === 'dark';
-  const buttonBgColor = isDarkMode ? 'hover:bg-gray-100 bg-white bg-opacity-15' : 'bg-white bg-opacity-20';
-  const buttonHoverBgColor = isDarkMode ? 'hover:bg-gray-100' : 'hover:bg-gray-300';
-  const drawerBgColor = isDarkMode ? 'bg-black' : 'bg-white';
-  const textColor = isDarkMode ? 'text-white' : 'text-black';
+  const buttonBgColor = isDarkMode ? '#2c2c2c' : '#f8f9fa'; // Adjusted colors for a modern look
+  const buttonTextColor = isDarkMode ? '#fff' : '#333';
+  const drawerBgColor = isDarkMode ? '#000000' : '#ffffff'; // Subtle background colors
+  const iconColor = isDarkMode ? '#ccc' : '#333';
 
   return (
     <>
       {/* Filter Button */}
-      <Button
-        onClick={open}
-        className={`border border-gray-700 rounded-lg ${textColor} ${buttonBgColor} ${buttonHoverBgColor}`}
-        variant="filled"
-      >
-        <span className="flex items-center">
-          <IconAdjustmentsHorizontal className="h-5 w-5 mr-2" />
-          <span className="font-medium">Filters</span>
-        </span>
-      </Button>
+      <Group position="right" mt="md">
+        <Button
+          onClick={open}
+          style={{
+            backgroundColor: buttonBgColor,
+            color: buttonTextColor,
+            border: isDarkMode ? '1px solid #444' : '1px solid #ddd',
+            transition: 'background-color 0.3s, color 0.3s',
+          }}
+          variant="filled"
+          size="md"
+          radius="lg"
+          leftIcon={<IconAdjustmentsHorizontal color={iconColor} size={18} />}
+        >
+          Filters
+        </Button>
+      </Group>
 
-      {/* Mantine Drawer */}
+      {/* Drawer Component */}
       <Drawer
         offset={8}
         radius="md"
         opened={opened}
         onClose={close}
-        size="md"
+        size="lg" // Increased size for better usability
         position="right"
         withCloseButton={false}
-        padding={0}
+        overlayOpacity={0.6}
+        overlayBlur={3}
+        padding="md"
         styles={{
-          content: { backgroundColor: drawerBgColor },
+          content: { backgroundColor: drawerBgColor, borderRadius: '16px' }, // Rounded corners for a smoother look
           header: { backgroundColor: drawerBgColor },
         }}
       >
-        <Filters filters={filters} setFilters={setFilters} ordering={ordering} setOrdering={setOrdering} />
+        <Filters
+          filters={filters}
+          setFilters={setFilters}
+          ordering={ordering}
+          setOrdering={setOrdering}
+        />
       </Drawer>
     </>
   );
